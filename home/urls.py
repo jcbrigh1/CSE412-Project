69e8372ab6f22a6cf4c72f43c81469bb1f64ef00
jcbrigh1 <61443967+jcbrigh1@users.noreply.github.com>
2021-11-24 14:23:47 -0700

    Add new web pages

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('searchName/', views.searchName),
+    path('searchPerson/', views.searchPerson),
+    path('searchGenreStyle/', views.searchGenreStyle),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'GET':
+        print("here 1")
+        return render(request, 'index.html')
+    if request.method == 'POST':
+        print("here 2")
+        if request.form['selection'] == 'name':
+            print("here 3")
+            return redirect('searchName')
+    #return render(request, 'index.html')
+
+def searchName(request):
+    return render(request, 'searchName.html')
+
+def search(request):
+    return render(request, 'search.html')
+
+def searchGenreStyle(request):
+    return render(request, 'searchGenreStyle.html')
+
+def searchPerson(request):
+    return render(request, 'searchPerson.html')
